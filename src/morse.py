76623ffe8696e82eacaa135b4994334b4793bda1
Sanchit <sanxchit@gmail.com>
2016-03-30 06:23:06 +0530

    Added additional twitter handle

--- src/morse.py
+++ src/morse.py
@@ -17,7 +17,12 @@ MORSE_ALPHABET = {'A': '.-',     'B': '-...',   'C': '-.-.',
                   '6': '-....',  '7': '--...',  '8': '---..',
                   '9': '----.',
                   ' ': '/',  # Turn spaces between words into slashes.
-                  '@56ek ': '......'
+                  '@56ek ': '......',
+		  '@myalphabet':'-.-.-.-.-'
+
+
+
+
                   }
 
 INVERSE_MORSE_ALPHABET = dict((v, k) for (k, v) in MORSE_ALPHABET.items())
